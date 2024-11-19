43944d7ebe5278adb7044370589ebf5f92d0dd80
Tomáš Mîčka <tomas.micka@logmanager.com>
2024-11-19 12:51:29 +0100

    fix neovim

--- script.sh
+++ script.sh
@@ -6,12 +6,8 @@ echo "UPDATING..."
 apt update
 
 echo "INSTALLING"
-apt install gcc g++ git fzf -y
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-git clone https://github.com/to23mas/.nvim.git
-rm -rf /opt/nvim
-tar -C /opt -xzf nvim-linux64.tar.gz
-export PATH="$PATH:/opt/nvim-linux64/bin"
+apt install gcc g++ git fzf snapd -y
+snap install nvim --classic
 
 echo "Setting up aliases"
 curl -o "$TEMP_ALIASES_FILE" https://raw.githubusercontent.com/to23mas/dev-tools/refs/heads/main/aliases
@@ -19,6 +15,6 @@ source "$TEMP_ALIASES_FILE"
 
 echo "Setting up neovim"
 git clone https://github.com/to23mas/.nvim.git
-mkdir .config/
-mv .nvim ./.config/nvim 
+mkdir -p /home/root/.config/
+mv .nvim /home/root/.config/nvim
 nvim .
