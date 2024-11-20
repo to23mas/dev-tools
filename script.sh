d642740d7e49d6025ce755d06a8c7585841daeda
Tomáš Míčka <tomas.micka@logmanager.com>
2024-11-20 08:17:22 +0100

    tmux added

--- script.sh
+++ script.sh
@@ -10,11 +10,17 @@ apt install gcc g++ git fzf snapd -y
 snap install nvim --classic
 
 echo "Setting up aliases"
-curl -o "$TEMP_ALIASES_FILE" https://raw.githubusercontent.com/to23mas/dev-tools/refs/heads/main/aliases
-source "$TEMP_ALIASES_FILE"
+curl -o aliases https://raw.githubusercontent.com/to23mas/dev-tools/refs/heads/main/aliases
+source aliases
+
+echo "Setting up tmux"
+curl -o tmux.conf https://raw.githubusercontent.com/to23mas/dev-tools/refs/heads/main/tmux.conf
+mkdir -p /home/root/.config/tmux/
+mv tmux.conf /home/root/.config/tmux/
 
 echo "Setting up neovim"
 git clone https://github.com/to23mas/.nvim.git
 mkdir -p /home/root/.config/
 mv .nvim /home/root/.config/nvim
-nvim .
+
+export CI_REGISTRY=registry.gitlab.logmanager.cz
